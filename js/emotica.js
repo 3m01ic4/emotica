5bb3f242e83283777ba604b5a449efc19117a1dc
Ka <k@ka.st>
2018-09-20 21:55:05 +0000

    First more or less real proto

--- js/emotica.js
+++ js/emotica.js
@@ -1,67 +1,110 @@
-$( document ).ready(function() {
+/* global $ Typed */
+
+$(document).ready(function() {
 
   $("#emotions").hide();
+  $("#emoticaSide").hide();
+
+  var lastScroll = 0;
+  var emoticaBrandY = $('#emoticaBrand').position().top;
   
   var options = {
-  strings: ["E^500motica"],
-  typeSpeed: 600,
-  contentType: 'html',
-  showCursor: false,
-  autoInsertCss: false
-}
+    strings: ["E^500motica"],
+    typeSpeed: 600,
+    contentType: 'html',
+    showCursor: false,
+    autoInsertCss: false
+  }
 
   var typed = new Typed("#emoticaBig", options);
 
-  setTimeout(function() {$("#emotions").fadeIn(3000)},5500);
+  setTimeout(function() { $("#emotions").fadeIn(3000) }, 5500);
 
+  $.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
   
-var editBtn = document.getElementById('editBtn');
-var editables = document.querySelectorAll('#about, #coachig, #contact')
- 
-editBtn.addEventListener('click', function(e) {
-  if (!editables[0].isContentEditable) {
-    editables[0].contentEditable = 'true';
-    editables[1].contentEditable = 'true';
-    editables[2].contentEditable = 'true';
-    //editables[3].contentEditable = 'true';
-
-    editBtn.innerHTML = 'Save Changes';
-    editBtn.style.backgroundColor = '#6F9';
-  } else {
-    // Disable Editing
-    editables[0].contentEditable = 'false';
-    editables[1].contentEditable = 'false';
-    editables[2].contentEditable = 'false';
-    //editables[3].contentEditable = 'false';
-    // Change Button Text and Color
-    editBtn.innerHTML = 'Enable Editing';
-    editBtn.style.backgroundColor = '#F96';
-    // Save the data in localStorage 
-    for (var i = 0; i < editables.length; i++) {
-      localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
-    }
-  }
-});  
-
-if (typeof(Storage) !== "undefined") {
- 
-  if (localStorage.getItem('about') !== null) {
-    editables[0].innerHTML = localStorage.getItem('about');
-  }
-   
-  if (localStorage.getItem('coaching') !== null) {
-    editables[1].innerHTML = localStorage.getItem('coaching');
-  }
-   
-  if (localStorage.getItem('contact') !== null) {
-    editables[2].innerHTML = localStorage.getItem('contact');
+  function onResize() {
+     if ($(window).width()<1040) {
+       $("#emoticaTopLink").html('<i class="fas fa-angle-up"></i><br/>E');
+       $("#emoticaSide").css("left","5px");
+     } else {
+       $("#emoticaTopLink").html('Emotica <i class="fas fa-angle-up"></i>');
+       $("#emoticaSide").css("left","35px");
+     }
+     if ($(window).width()<815) {
+       $("#emoticaSide").hide();
+     } else {
+       $("#emoticaSide").show();
+     }
   } 
- // if (localStorage.getItem('mainText') !== null) {
-  //  editables[3].innerHTML = localStorage.getItem('mainText');
-//  } 
+
+  function onScroll() {
+    var headerH = $('#crowd').outerHeight(true);
+   //this will calculate header's full height, with borders, margins, paddings
+   var scrollTopVal = $(this).scrollTop();
+    if ( scrollTopVal >= 0  && scrollTopVal + $('#emoticaBig').outerHeight(false)  <= headerH  ) {
+        $('#emoticaBrand').css({'position':'fixed','top' :emoticaBrandY});
+        $("#navbarTopEmotica").addClass("fixed-top");
+        $("#navbarTopEmotica").show();
+    } else {
+        $('#emoticaBrand').css({'position':'absolute','top':emoticaBrandY});
+        $("#navbarTopEmotica").hide();
+        $("#navbarTopEmotica").removeClass("fixed-top");
+    }
+    
+    $('#emoticaBig').each(function() {
+      if (!$(this).isInViewport()) {
+        $("#emoticaSide").show();
+      } else {
+        $("#emoticaSide").hide();
+      }
+    });
+    
+    $("#crowd").each(function() {
+      if (!$(this).isInViewport()) {
+        $(".nav-link").addClass("redmenu");
+      } else {
+        $(".nav-link").removeClass("redmenu");
+      }
+    });
+  }
   
-}
+  $(window).on('resize', function() {
+    onResize();   
+  });
 
+  $(window).on('scroll', function() {
+    onScroll();
+  });
 
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  onResize();
+  onScroll();
 });
-
